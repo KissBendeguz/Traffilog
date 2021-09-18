6089aee2507155c29717856d0b8f5f288a85dcc9
Bendi <Bendi@DESKTOP-0D84MSB>
2021-09-18 19:00:12 +0200

    Add project files.

---
+++ Traffilogg/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿
+using Microsoft.Win32;
+using System;
+using System.Windows.Forms;
+
+namespace Traffilogg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        /// 
+
+        
+        private void InitializeComponent()
+
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "log";
+            this.openFileDialog1.FileName = "console.log";
+            this.openFileDialog1.Filter = "Log files (*.log)|*.log|Text files(*.txt)|*.txt|All files (*.*)|*.*";
+            this.openFileDialog1.Title = "Válaszd ki a log file-t.";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.White;
+            this.textBox1.Location = new System.Drawing.Point(40, 85);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(400, 285);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.DimGray;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(40, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(400, 26);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(55, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(371, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Írd be a neveket (akár vesszővel elválasztva többet)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(124)))), ((int)(((byte)(197)))), ((int)(((byte)(103)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(150, 390);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 40);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Kiértékelés";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(480, 457);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Traffi log";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Button button1;
+    }
+}
+
---
+++ Traffilogg/Form1.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Traffilogg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+ 
+            InitializeComponent();
+            
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            string line="";
+            string preLine="";
+            string[] names = textBox2.Text.Split(',');
+
+
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file = new System.IO.StreamReader(openFileDialog1.FileName);
+            int money = 0,
+                totalspeed = 0,
+                counter = 0,
+                maxmoney = 0,
+                maxspeed=0;
+
+            while ((line = file.ReadLine()) != null)
+            {
+                bool linecontains = false;
+                bool prelinecontains = false;
+                foreach (string name in names) {
+                    if (line.Contains(name)) {
+                        linecontains = true;
+                        break;
+                    }
+                    if (preLine.Contains(name))
+                    {
+                        prelinecontains = true;
+                        break;
+                    }
+                }
+                
+                if (line.Contains("[SeeMTA - Traffipax]:") && linecontains && line.EndsWith("$"))
+                {
+                    string[] splitted = line.Split(' ');
+                    int currentmoney = int.Parse(splitted[splitted.Length - 1].Replace("$", ""));
+                    money += currentmoney;
+
+                    if (currentmoney > maxmoney) { maxmoney = currentmoney; }
+
+                    counter++;
+
+                }
+
+                
+                if (line.Contains("[SeeMTA - Traffipax]:") && prelinecontains && line.EndsWith("KM/h"))
+                {
+                    string[] splitted = line.Split(' ');
+                    int limit = int.Parse(splitted[splitted.Length - 1].Replace("KM/h", ""));
+                    int speed = int.Parse(splitted[splitted.Length - 4].Split('.')[0]);
+
+                    totalspeed += speed - limit;
+
+                    if (speed > maxspeed) { maxspeed = speed; }
+                }
+                preLine = line;
+            }
+
+            file.Close();
+            if (counter == 0) {
+                MessageBox.Show("Nem található rekord a megadott paraméterekkel.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            textBox1.Text = "";
+            textBox1.Text +=    "Büntetések száma: " + counter + Environment.NewLine +
+                                "Büntetések összege: " + money + "$" + Environment.NewLine +
+                                "Legmagasabb büntetés: " + maxmoney + "$" + Environment.NewLine +
+                                "Legmagasabb sebesség: " + maxspeed.ToString("#.##") + "KM/h" + Environment.NewLine +
+                                "Átlagos büntetés: " + ((double)money / counter).ToString("#.##") + "$" + Environment.NewLine +
+                                "Átlagos sebességhatár átlépés: " + ((double)totalspeed / counter).ToString("#.##") + "KM/h";
+
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MaximizeBox = false;
+            MinimizeBox = false;
+            
+
+            string pathName = "";
+
+            RegistryKey registryKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\WOW6432Node\\Multi Theft Auto: San Andreas All\\1.5");
+            if (registryKey != null)
+            {
+                pathName = (string)registryKey.GetValue("Last Run Location") + "/MTA/logs";
+            }
+            else
+            {
+                pathName = "c:\\";
+            }
+
+            this.openFileDialog1.InitialDirectory = pathName;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (textBox2.Text.Equals(""))
+            {
+                MessageBox.Show("Adj meg legalább egy nevet!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else {
+                openFileDialog1.ShowDialog();
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
